e5e411345a54738475b95e13c7212cf4af09dd1e
limkimhuor <kimhuorlim@gmail.com>
2015-04-25 14:44:09 +0700

    Finish login/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Blog App"
       redirect_to @user
       # Handle a successful save.
