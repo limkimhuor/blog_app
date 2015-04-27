5c0663206f9d362fea4da1e2bab2f03e351163b4
limkimhuor <kimhuorlim@gmail.com>
2015-04-27 15:13:02 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,14 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
-
-  resources :entries
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :entries,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
